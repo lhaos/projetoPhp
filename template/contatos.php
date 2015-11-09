9c2bd6fd2209b1c2292f1b057606a414eb76b07f
Thiago <thisimaso@gmail.com>
2015-11-09 20:46:20 -0200

    Formulária criado e css atualizado com elementos para o formulário. Faltando criar o controle e a resposta

--- template/contatos.php
+++ template/contatos.php
@@ -4,7 +4,14 @@ include_once '/template/inc/head.php';
 ?>
     <div class="jumbotron">
         <h1>Contato</h1>
-        <p>Olá mundo!</p>
+        <form name="cad" id="cad" method="post" action="">
+            <input type="text" name="txtnome" id="txtnome" placeholder="Nome" class="q" required autofocus />
+            <input type="text" name="txtemail" id="txtemail"  placeholder="Email" class="q" required />
+            <input type="text" name="txtassunto" id="txtassunto"  placeholder="Assunto" class="q" required />
+            <textarea rows="3" cols="30" name="txtmensagem" id="txtmensagem" class="q" required>
+            </textarea>
+            <input type="submit" name="btnenviar" id="btnenviar" value="Enviar" class="q"/>
+        </form>
     </div>
 <?php
 include_once '/template/inc/footer.php';
