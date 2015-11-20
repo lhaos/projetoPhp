0f6ea75ba81bdb3a6534214ac359fdae7417786c
Thiago <thisimaso@gmail.com>
2015-11-20 17:56:42 -0200

    Modificações no index, agora é possivel acessar as paginas escrevendo diretamente na barra de navegação de forma simplificada e ainda assim tratando os erros. Para isso foi criado um array com os nomes possiveis da url e uma function para pegar a url e então passar pelos parametros e fazer as verificações

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <?php
     date_default_timezone_set("Brazil/East");
 
+    $rota = parse_url("http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+    $path = str_replace("/","",$rota['path']);
+
+    $url = ["contatos", "empresa", "produtos", "servicos"];
+
     if(isset($_GET['file'])){
         $file = $_GET['file'];
-    }else{
+    } else if($path == "" || $file != $_GET['file']){
         $file = "home";
+    }//fecha else
+
+    if($path == $url[0] || $path == $url[1] || $path == $url[2] || $path == $url[3]){
+        $file = $path;
+    }else if($file != "home" && $file != $path && $file != $_GET['file'] ){
+        $file = "error/404";
     }//fecha else
 
     if(file_exists("template/{$file}.php")){
         include_once "template/{$file}.php";
     }else{
-        include_once "template/error/404.php";
+        $file = "error/404";
+        include_once "template/{$file}.php";
     }//fecha else
